1bfa316edb8f9c09349aa2a3a4edfc10b63b7451
songliangliang <18610090485@163.com>
2017-04-19 20:30:19 +0800

    增加新方法

--- src/liang/Test.java
+++ src/liang/Test.java
@@ -1,7 +1,13 @@
 package liang;
 
+import java.lang.reflect.Method;
+
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("i can");
+	}
+	private void method1() {
+		// TODO Auto-generated method stub
+		System.out.println("lianxi");
 	}
 }
